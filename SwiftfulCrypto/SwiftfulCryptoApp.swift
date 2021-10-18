9dcf853e99dfa425e87a50bde942525552e44ea8
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-10-18 08:21:45 +0200

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Andre Dippenaar on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
