536f807ed8f4aba36ee9376f42e188151ed5f664
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-10-19 14:43:06 +0200

    Add dynamuc List to display downloaded coins

---
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  SwiftfulCrypto
+//
+//  Created by Andre Dippenaar on 2021/10/19.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
